886141fa1ef63ec838dbb30868cf413202940e75
jokezer <jokezer@gmail.com>
2014-02-16 14:15:46 +0400

    First commit in GTD prototype repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gtd::Application.config.secret_key_base = '5bda7693adf21f4019a2f7ad0ad2bee5c4e642b6fe6747af8ac2cccedf692487c465078fe9422b7e15a84ff717f49185d7675033b5782e88fa9d610d19c658a7'
