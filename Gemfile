00eb5a9c60eaa7c1908f2b0adc4aef74e08722ba
jokezer <jokezer@gmail.com>
2014-04-03 20:12:10 +0400

    init backbone.js

--- Gemfile
+++ Gemfile
@@ -33,6 +33,9 @@ gem 'coffee-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
+#Backbone.js
+gem "rails-backbone"
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 gem 'jquery-ui-rails'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,17 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require underscore
+//= require backbone
+//= require backbone_rails_sync
+//= require backbone_datalink
+//= require backbone/gtd
 //= require_tree .
 //= require jquery.ui.all
 //= require bootstrap
+//= require underscore
+//= require backbone
+//= require backbone_rails_sync
+//= require backbone_datalink
+//= require backbone/gtd
 //= require_tree ./plugins/
